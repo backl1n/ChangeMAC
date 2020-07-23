474b9f047a54a372c751c45b1f4ea913df8d7940
kali <kali@kali>
2020-07-23 16:33:30 +0200

    modification des conditions

--- chgMAC.sh
+++ chgMAC.sh
@@ -6,7 +6,7 @@ echo "------------------------------------------"
 echo "inteface:"
 sudo ifconfig
 echo "----------------------------------------------"
-echo "choisir interface"
+echo "choisir interface :"
 read interface
 echo "----------------------------------------------"
 sudo ifconfig $interface down
@@ -16,20 +16,20 @@ echo "choisir l'option : "
 read option
 echo "----------------------------------------------"
 #!--------------------------------------------------------
-if [[$option = '1']]
+if (($option == '1')); 
 then
     echo "choix 1:"
     sudo macchanger -e $interface
-elif [[$option = '2']]
+elif (($option == '2')); 
+then 
     echo "choix 2:"
     echo "nouvelle adresse mac ?(01:02:03:04:05:06)"
     read $adresse_mac
     sudo macchanger --mac=$adresse_mac
 
-else [[$option != '1' || $option != '2']]
+else #[[$option != 1 || $option != 2]]
     echo "###################"
     echo "error: bad choice"
     echo "###################"
-
 fi 
 ifconfig $interface up
